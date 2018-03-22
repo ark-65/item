79b9ca7be646357174213cb87599eb9af0c5f87b
ark_65 <345549325@qq.com>
2018-03-22 20:59:20 +0800

    用for循环打印了菱形

--- src/ProcessControl/PracticeTwo.java
+++ src/ProcessControl/PracticeTwo.java
@@ -9,9 +9,26 @@ package ProcessControl;
  */
 public class PracticeTwo {
     public static void main(String[] args) {
-        for (int i = 0; i < 7; i++) {
-
-
+        for (int i=1;i<=5;i++) {
+            for (int j=1;j<=5-i;j++) {
+                System.out.print(" ");
+            }
+            for (int k = 1;k<=(2*i-1);k++){
+                System.out.print("*");
+            }
+            System.out.println(" ");
         }
+        for (int i=1;i<=4;i++) {
+            //空格 1 2 3 4 so
+            for(int j=1;j<=i;j++) {
+                System.out.print(" ");
+            }
+            //* 7 5 3 1倒着来的基数
+            for(int k=7;k>=2*i-1;k--){
+                System.out.print('*');
+            }
+            System.out.println();
+        }
+
     }
 }
